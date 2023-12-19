4975d7de4eb37042c1a046ddcfbdcc939be35694
MacPhuu <macphuzz@gmail.com>
2023-12-19 21:29:20 +0700

    embe

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-
+import moment from 'moment'
 
 window.onload = function(){
-    const get_day_of_time = (d1, d2) => {
-    let ms1 = d1.getTime();
-    let ms2 = d2.getTime();
-    return Math.ceil((ms2 - ms1) / (24*60*60*1000));
-};
-
-let loveday = new Date('2023-09-26');
-
-//start
-let today = new Date();
-;
-//now
-
-let time = get_day_of_time(loveday,today);
-
-const days = document.getElementById("days");
+    // Get the start date (September 16, 2022)
+    const startDate = moment('2023-09-26');
+    
+    // Get the current date
+    const currentDate = moment();
+    
+    // Calculate the difference in days
+    const time = currentDate.diff(startDate, 'days');
 days.innerHTML="Yêu em bé "+time+" ngày";
 }
